f9f5fce182f6d40954884941fff0d1c7a28a575a
Klymenko Nazariy <nazariy.klymenko@gmail.com>
2023-03-02 18:24:43 +0200

    learning js

---
+++ JavaScriptLearn/js_3_types/js/js_3_types.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let numberOfToys = 2;//number type
+console.log(`Number of toys = ${numberOfToys}`);// "`" is grave accent, sreing with additional functionality
+
+let bigIntNumber = 1n;
+console.log(typeof(bigIntNumber));
+console.log(bigIntNumber);
