a90ec76b1509900fde033dbcee9578083f1b7e3f
Klymenko Nazariy <nazariy.klymenko@gmail.com>
2023-03-03 10:28:02 +0200

    types learning

--- JavaScriptLearn/js_3_types/js/js_3_types.js
+++ JavaScriptLearn/js_3_types/js/js_3_types.js
@@ -1,8 +1,22 @@
 'use strict';
+let NaNVar = NaN;//NaN type
+console.log(typeof(NaNVar));
 
 let numberOfToys = 2;//number type
 console.log(`Number of toys = ${numberOfToys}`);// "`" is grave accent, sreing with additional functionality
 
-let bigIntNumber = 1n;
-console.log(typeof(bigIntNumber));
+let bigIntNumber = 1n;//bigint type
+console.log(typeof (bigIntNumber));
 console.log(bigIntNumber);
+
+let stringName = "Nazariy";//string type
+console.log(stringName);
+
+let boolIsGreater = 1488 > 1337;//boolean type
+console.log(boolIsGreater);
+
+let voidVar = null;//null type
+console.log(typeof(voidVar));
+
+let variable;//undefined type
+console.log(variable);
