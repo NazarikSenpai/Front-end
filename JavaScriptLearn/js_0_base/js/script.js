2e1e8c6e869ae0f4c5182a820ff70988d1ff1049
Klymenko Nazariy <nazariy.klymenko@gmail.com>
2023-03-04 21:52:30 +0200

    learning js 4, 5, 6

---
+++ JavaScriptLearn/js_0_base/js/script.js
@@ -0,0 +1,31 @@
+'use strict';
+let name = true;
+console.log("name =", name, "\ntype of name:", typeof (name));
+name = String(name);//string conversion
+console.log("name =", name, "\ntype of name:", typeof (name));
+
+/* !!!!!!!!!! */
+console.log(Number(undefined));         // NaN, conversion error 
+/* !!!!!!!!!! */
+
+console.log(Number(" 234 "));           // 234
+console.log(Number(true));              // 1
+console.log(Number(false));             // 0
+console.log(Number("hi there 321"));    // NaN, conversion error
+console.log(Number(null));              // 0
+
+console.log(Boolean(0));                //false
+console.log(Boolean(NaN));              //false
+console.log(Boolean(undefined));        //false
+console.log(Boolean(null));             //false
+console.log(Boolean(""));               //false
+console.log(Boolean(123));              //true
+console.log(Boolean(1));                //true
+console.log(Boolean("HELLO"));          //true
+
+/* !!!!!!!!!! */
+console.log(Boolean("   "));            //true
+console.log(Boolean("0"));              //true
+/* !!!!!!!!!! */
+
+console.log(typeof(alert));
---
+++ JavaScriptLearn/js_6_operators/js/script.js
@@ -0,0 +1,60 @@
+'use strict';
+console.log(10 % 4);//оператор остачі
+
+console.log(2 ** 6);//оператор піднесення до степеня
+console.log(625 ** (1 / 2));
+let root = 21 ** (1 / 3);//2.7589241763811203
+console.log(root);
+
+console.log("2" + "3");//"23"
+console.log(2 + "3");//"23"
+console.log(2 + 1 + 3 + "4" + 5);//"645"
+console.log(6 - "2");//4
+console.log(6 / "2");//3
+
+console.log(+true);//1 унарний оператор +
+console.log(+"")//0
+
+let a = 3;
+let b = 5;
+let c = 1 + 2 + (a = b - 1);//1+2+4   a=4
+console.log("c =", c);
+
+let x, y, z;
+x = y = z = 4;//так краще не робити
+z = 4;//краще робити так
+y = z;
+x = z;
+console.log("x =", x);
+
+let counter = 3;
+let size = ++counter;//префіксний інкремент
+console.log("size =", size);//4
+
+let counter1 = 3;
+let size1 = counter1++;//постфіксний інкремент
+console.log("size1 =", size1);//3
+
+a = 1 + 2, 1 + 5;//оператор кома має найнижчий пріоритет
+console.log(a);//3
+a = (1 + 2, 1 + 5);
+console.log(a);//6
+
+let string1 = "Fart";//k>t
+let string2 = "Fark";
+console.log("string1 > string2:", string1 > string2);//true
+
+a = 0;
+b = "0";
+console.log("bool a =", Boolean(a));//false
+console.log("bool b =", Boolean(b));//true
+console.log("a==b =", a == b);//TRUE!
+
+console.log("0===false =", 0 === false);//=== оператор строгої рівності
+
+console.log("null==undefined =", null == undefined);//true
+console.log("null===undefined =", null === undefined);//false
+console.log("null==0 =", null == 0);//false
+console.log("null>0 =", null > 0);//false
+console.log("null>=0 =", null >= 0);//true
+console.log("Number(undefined) =", Number(undefined));//NaN   
