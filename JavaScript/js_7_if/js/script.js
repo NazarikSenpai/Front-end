edb8230bc0b5d8bffc4c9292d8b888698c85d231
Klymenko Nazariy <nazariy.klymenko@gmail.com>
2023-04-10 13:58:03 +0300

    js if statement and logic operators learning

---
+++ JavaScript/js_7_if/js/script.js
@@ -0,0 +1,38 @@
+'use strict';
+// let age = prompt("How old are you?", '');
+// let isAdult = (age >= 18);
+// // if (isAdult) {
+// //     alert("Wow, you are adult!");
+// // } else {
+// //     alert("Oh, you are still a kid.");
+// // }
+// isAdult ? alert("Wow, you are adult!") : alert("Oh, you are still a kid.");
+
+// let answer = prompt("Which company made JS?", '');
+// if (answer == 'Netscape') {
+//     alert("You are right");
+// } else {
+//     alert("No, you are wtrong");
+// }
+
+// let userNumber = prompt("Write number", '');
+// if (userNumber > 0) {
+//     alert(1);
+// } else if (userNumber < 0) {
+//     alert(-1);
+// } else if (userNumber == 0) {
+//     alet(0);
+//}
+
+// let a = prompt("Write 'a' number", 0);
+// let b = prompt("Write 'b' number", 0);
+// let result;
+
+// a + b < 4 ? result = "lower" : result = "higher";
+// alert(result);
+
+// let login = prompt("Write a message", '');
+
+// login == "Director" ? alert("Hello!") :
+//     login == "Employee" ? alert("Hi!") :
+//         login == "" ? alert("No login");
---
+++ JavaScript/js_8_logicOperators/script.js
@@ -0,0 +1,63 @@
+"use strict"
+// console.log(true || true);
+// console.log(false || true);
+// console.log(true || false);
+// console.log(false || false);
+
+// let currentHour = 20;
+// let isWeekend = false;
+
+// if (currentHour < 10 || currentHour > 22 || isWeekend) {
+//     console.log("We are closed now");
+// }
+
+// let firsName = 'Nazariy';
+// let lastName = '';
+// let nickname = 'br0gy4g4!!!228';
+
+// console.log(firsName || lastName || nickname);
+// true || console.log('Doesn`t logged');
+// false || console.log('Logged');
+
+// console.log(23 && 24);
+// let currentHour = 13;
+// let currentMinute = 54;
+// if (currentHour == 12 || currentHour == 13 && currentMinute == 47 || currentMinute == 54) {
+//     console.log('Time is 12:47 or 13:54 now =)');
+// } else {
+//     console.log('Your time isn`t 12:47 or 13:54');
+// }
+
+// let stringOfNumbers = "423234432";
+// console.log(!!stringOfNumbers);
+
+// let age = 13;
+// if (age >= 14 && age <= 90) {
+//     console.log('Your age is between 14 and 90');
+// }
+// if (!(age >= 14 && age <= 90)) {
+//     console.log('Your age isn`t between 14 and 90');
+// }
+// if (age > 14 || age < 90) {
+//     console.log('Your age isn`t between 14 and 90');
+// }
+let password = "TheMaster";
+let userLogin = prompt("Login:", '');
+let userPassword = '';
+if (userLogin === "Admin") {
+
+    userPassword = prompt("Password:", '');
+
+    if (userPassword === password) {
+        alert("Welcome!");
+    } else if (userPassword != null) {
+        alert("Wrong password");
+    }
+
+} else if (userLogin != null) {
+    alert("I don`t know you");
+}
+
+if (userLogin == null || userPassword == null) {
+    alert("Canseled");
+}
