798d38b3029e67bc57023ef7ddaa410beb4c3f75
Klymenko Nazariy <nazariy.klymenko@gmail.com>
2023-04-13 20:43:34 +0300

    js arrow function basics learn

---
+++ JavaScript/js_14_arrow_function/script.js
@@ -0,0 +1,30 @@
+"use strict"
+// let summ = (firstNumber, secondNumber) => firstNumber + secondNumber;
+
+// console.log(summ(23, 32));
+
+// let age = 23;
+// let welcome = (age < 18) ? () => alert("Hi!") : () => alert("Good afternoon!");
+
+// welcome();
+
+//Task:
+// function ask(question, yes, no) {
+//     if (confirm(question)) yes()
+//     else no();
+//   }
+
+//   ask(
+//     "Вы согласны?",
+//     function() { alert("Вы согласились."); },
+//     function() { alert("Вы отменили выполнение."); }
+//   );
+let ask = (question, yes, no) => {
+    return confirm(question) ? yes() : no();
+}
+
+ask(
+    "Are you agree?",
+    () => alert("You agreed"),      //lol, this works =D
+    () => alert("You desagreed")
+)
