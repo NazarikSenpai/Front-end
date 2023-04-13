ae2ab9b247d5f7cb71b419143ed962118a02b515
Klymenko Nazariy <nazariy.klymenko@gmail.com>
2023-04-13 20:30:30 +0300

    js function expression learn

---
+++ JavaScript/js_13_function_expression/script.js
@@ -0,0 +1,60 @@
+"use strict"
+//Function expression:
+// let sayHi = function(){
+//     console.log("Hi, there!");
+// };
+
+// let sayHi2 = sayHi;
+
+// alert(sayHi2);
+
+//Function callback:
+// function ask(question, yes, no) {    //function declaration
+//     if (confirm(question)) { yes(); }
+//     else { no(); }
+// };
+
+// function showYes() {   //function callback, function expression
+//     alert("You said yes");
+// };
+
+// let showNo = function () {    //function callback, function expression
+//     alert("You said no");
+// };
+
+// ask("Do you agree?", showYes, showNo);
+
+//Function expression example
+let age = 27;
+let welcome;
+
+//Variaton of welcome declaration:
+// let welcome = (age < 18) ?
+//     function () { alert("Hi!"); } :
+//     function () { alert("Hi!"); };
+
+if (age < 18) {
+
+    function welcome() {
+        alert("Привет!");
+    }
+
+} else {
+
+    function welcome() {
+        alert("Здравствуйте!");
+    }
+
+}
+
+// if (age < 18) {
+//     welcome = function () {
+//         alert("Hi!");
+//     };
+// } else {
+//     welcome = function () {
+//         alert("Good afternoon!");
+//     };
+// }
+
+welcome();
