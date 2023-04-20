3a886cfda32a6a39a67b72e23ce147f95c7eb411
Klymenko Nazariy <nazariy.klymenko@gmail.com>
2023-04-21 00:21:05 +0300

    calculator task added

---
+++ JavaScript/Objects_Basics/js_19_constructor/calculator.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+"use strict";
+function Calculator() {
+  this.firstNumber = 0;
+  this.secondNumber = 0;
+
+  this.read = function () {
+    this.firstNumber = +prompt("Enter first number:", 0);
+    this.secondNumber = +prompt("Enter second number:", 0);
+  };
+
+  this.sum = function () {
+    return this.firstNumber + this.secondNumber;
+  };
+
+  this.mul = function () {
+    return this.firstNumber * this.secondNumber;
+  };
+}
