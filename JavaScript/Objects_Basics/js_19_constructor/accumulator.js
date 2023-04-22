6e911e6a42064be4072da52007e74f94e219e107
Klymenko Nazariy <nazariy.klymenko@gmail.com>
2023-04-22 13:40:43 +0300

    js 20 optional chain learn

--- JavaScript/Objects_Basics/js_19_constructor/accumulator.js
+++ JavaScript/Objects_Basics/js_19_constructor/accumulator.js
@@ -1,7 +1,10 @@
 "use strict";
 function Accumulator(startingValue) {
   this.value = startingValue;
-  this.read = function () {
-    this.value += +prompt("Enter the value:", 0);
-  }
+  this.read = function (value) {
+    this.value += (value || +prompt("Enter the value:", 0));
+  };
 }
+
+let accum = new Accumulator(20);
+accum.read();
---
+++ JavaScript/Objects_Basics/js_20_optional_chain/script.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-unused-vars */
+"use strict";
+
+/* Проблема відсутньої властивості */
+let user = {
+  street: "lorem",
+  age: 30,
+  contacts: {
+    phone: {
+      number: 12345667788,
+      countryCode: "+380",
+    },
+  },
+};
+console.log(user.contacts && user.contacts.phoneNumber);//перевірка на існування через логічний оператор "І"
+
+/* Опціональний ланцюжок */
+console.log(user?.contacts?.phone?.countryCode);
+let value = 42;
+console.log(value?.longitude?.size);//Працює також і з не об'єктами
+
+/* Скорочене обчислення */
+let properties = null;
+let number = 1;
+function addOne(number){
+  this.number += number;
+}
+properties?.addOne(number++);//Виконання не дійде до функції через опціональний ланцюжок
+console.log(number);          
+
+/* Застосування з методами та квадратними дужками ?.() ?.[] */
+let sportcar = {
+  brand:"Ferrari",
+  sound() {
+    console.log("WRUM-WRUM!!!");
+  },
+};
+
+let car = {
+  wheels: 4,
+};
+
+let bicycle = null;
+
+sportcar.sound?.();
+car.sound?.();
+
+let key = "brand";
+
+console.log(sportcar?.[key]);
+console.log(bicycle?.[key]);
+
+/* Свої приклади */
+
+let userCurrent = {
+  phone: 0662477693,
+  country: {
+    state: "Ukraine",
+    code: 380,
+  },
+
+}
+
+function phoneCheck(userObject) {
+  userObject.phone?.
+}
