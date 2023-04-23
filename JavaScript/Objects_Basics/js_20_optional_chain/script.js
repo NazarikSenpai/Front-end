6ab09ebb61feedb30b874b91599afa0b7195bcd5
Klymenko Nazariy <nazariy.klymenko@gmail.com>
2023-04-23 22:05:09 +0300

    js 20 optional chain and js 21 symbols learn

--- JavaScript/Objects_Basics/js_20_optional_chain/script.js
+++ JavaScript/Objects_Basics/js_20_optional_chain/script.js
@@ -1,66 +1,67 @@
 /* eslint-disable no-unused-vars */
 "use strict";
 
-/* Проблема відсутньої властивості */
-let user = {
-  street: "lorem",
-  age: 30,
-  contacts: {
-    phone: {
-      number: 12345667788,
-      countryCode: "+380",
-    },
-  },
-};
-console.log(user.contacts && user.contacts.phoneNumber);//перевірка на існування через логічний оператор "І"
+// /* Проблема відсутньої властивості */
+// let user = {
+//   street: "lorem",
+//   age: 30,
+//   contacts: {
+//     phone: {
+//       number: 12345667788,
+//       countryCode: "+380",
+//     },
+//   },
+// };
+// console.log(user.contacts && user.contacts.phoneNumber);//перевірка на існування через логічний оператор "І"
 
-/* Опціональний ланцюжок */
-console.log(user?.contacts?.phone?.countryCode);
-let value = 42;
-console.log(value?.longitude?.size);//Працює також і з не об'єктами
+// /* Опціональний ланцюжок */
+// console.log(user?.contacts?.phone?.countryCode);
+// let value = 42;
+// console.log(value?.longitude?.size);//Працює також і з не об'єктами
 
-/* Скорочене обчислення */
-let properties = null;
-let number = 1;
-function addOne(number){
-  this.number += number;
-}
-properties?.addOne(number++);//Виконання не дійде до функції через опціональний ланцюжок
-console.log(number);          
+// /* Скорочене обчислення */
+// let properties = null;
+// let number = 1;
+// function addOne(number){
+//   this.number += number;
+// }
+// properties?.addOne(number++);//Виконання не дійде до функції через опціональний ланцюжок
+// console.log(number);          
 
-/* Застосування з методами та квадратними дужками ?.() ?.[] */
-let sportcar = {
-  brand:"Ferrari",
-  sound() {
-    console.log("WRUM-WRUM!!!");
-  },
-};
+// /* Застосування з методами та квадратними дужками ?.() ?.[] */
+// let sportcar = {
+//   brand:"Ferrari",
+//   sound() {
+//     console.log("WRUM-WRUM!!!");
+//   },
+// };
 
-let car = {
-  wheels: 4,
-};
+// let car = {
+//   wheels: 4,
+// };
 
-let bicycle = null;
+// let bicycle = null;
 
-sportcar.sound?.();
-car.sound?.();
+// sportcar.sound?.();
+// car.sound?.();
 
-let key = "brand";
+// let key = "brand";
 
-console.log(sportcar?.[key]);
-console.log(bicycle?.[key]);
+// console.log(sportcar?.[key]);
+// console.log(bicycle?.[key]);
 
 /* Свої приклади */
 
-let userCurrent = {
-  phone: 0662477693,
-  country: {
-    state: "Ukraine",
-    code: 380,
-  },
+let tripInfo = {
+  destination: {
+    country:prompt("Введіть країну призначення:"),
+  }
+};
 
-}
+// tripInfo.destination.country = prompt("Введіть країну призначення:");
+// tripInfo.destination.city = prompt("*Введіть місто призначення:");
+// tripInfo.departureTime = prompt("*Введіть час відправлення:");
+// tripInfo.passangers = prompt("Введіть кількість пасажирів:");
 
-function phoneCheck(userObject) {
-  userObject.phone?.
-}
+let currentCity = tripInfo.destination?.country;
+console.log(currentCity);
---
+++ JavaScript/Objects_Basics/js_21_symbols/script.js
@@ -0,0 +1,93 @@
+/* eslint-disable no-unused-vars */
+"use strict";
+
+/* Символи */
+
+// let symbol1 = Symbol("id");//id це просто опис(ім'я) який не має ніякого значення
+// let symbol2 = Symbol("id");
+// console.log(symbol1 == symbol2);
+// //alert(symbol1); - Cannot convert a Symbol value to a string
+ 
+// console.log(toString(symbol1));
+// console.log(symbol1.description);
+
+/* Приховані властивості */
+
+// let user = {
+//   name:"Vitaliy",
+// };
+
+// let id = Symbol("id");
+
+// user[id] = 42;
+// console.log(user[id]);//ми можемо отримати доступ до даних, використовуючи символ як ключ
+
+// let name = Symbol("name");//інша програма може створити свій ідентифікатор і він ніяк не повпливає на наш об'єкт
+// user[name] = "their identificator";
+// console.log(user[name]);
+// console.log(user.name);//властивість залишилась тією ж самою
+
+/* Символи в літералі об'єкта */
+
+// let car = {
+//   brand: "BMW",
+//   class: "S",
+//   [id]: 123321123321,//створили символ всередені літерала об'єкта
+// };
+
+/* Символи ігноруються циклом for in */
+
+// let human = {
+//   hand: 123,
+//   leg: 321,
+//   head: 23,
+//   [name]: "Nazarii",
+// };
+// for (const key in human) {
+//   console.log(`${key}:`,human[key]);//name не виводиться
+// }
+
+// console.log(Object.keys(human));//name також ігнорується
+
+// let humanClone = Object.assign(human);//а тут символ скопіювався тому що у цьому суть методу assign
+// console.log(humanClone);
+
+/* Глобальні символи */
+
+// let id = Symbol.for("id");//for шукає символи з іменем name у глобальному регістрі і повертає ТОЙ ЖЕ САМИЙ якщо знахожить
+// console.log(id.description);
+
+// let idClone = Symbol.for("id");
+// console.log(idClone.description);
+// console.log(id === idClone);
+
+/* Symbol.keyFor */
+
+// let symbol1 = Symbol.for("key1");
+// let symbol2 = Symbol.for("key2");
+
+// console.log(Symbol.keyFor(symbol1));//keyFor повертає значення глобального символа за його ім'ям
+// console.log(Symbol.keyFor(symbol2));
+
+// let globalsym = Symbol.for("xsym");
+// let localsym = Symbol("xsym");
+
+// console.log(Symbol.keyFor(globalsym));//працює тільки для глобальних об'єктів
+// console.log(Symbol.keyFor(localsym));
+
+/* Системні символи */
+
+let user = {};
+
+let toPrimitive = Symbol.toPrimitive;
+
+user[toPrimitive] = 123;
+console.log(user[toPrimitive]);
+
+let car = {};
+let sym = Symbol.for("id");
+car[sym] = 123;
+console.log(car[sym]);
+
+let symClone = Symbol.for("id");
+console.log(car[symClone]);
