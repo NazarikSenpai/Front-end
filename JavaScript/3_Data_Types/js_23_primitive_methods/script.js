7afd6e02321bdb6d2c60eb8b5e93faed3d8da0a0
Klymenko Nazariy <nazariy.klymenko@gmail.com>
2023-04-25 09:49:47 +0300

    js 23 primitive methods

---
+++ JavaScript/3_Data_Types/js_23_primitive_methods/script.js
@@ -0,0 +1,25 @@
+"use strict";//через це остання задача буде видавати помилку
+
+/* Примітиви як об'єкти */
+
+let user = {
+  name: "Пилип",
+  money: 2345.56,
+  sayHi() {
+    console.log(this.name.toUpperCase().repeat(10));
+  },
+};
+
+user.sayHi();
+
+console.log(user.money.toFixed(1));
+
+console.log(typeof 123);
+console.log(typeof new Number(123));//явне використання конструктора об'єкта-обгортки НЕ РЕКОМЕНДОВАНО
+
+// console.log(null.toUppercase());//
+// console.log(undefined.toUppercase());//у null і undefined немає методів
+
+// let userName = "Polina";
+// userName.age = 42;
+// console.log(userName.age = 42,userName.age == 4);
