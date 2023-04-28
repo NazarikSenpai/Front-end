794a00b6faa527a8daae2cbefd3d2f4d4b3e0d8c
Klymenko Nazariy <nazariy.klymenko@gmail.com>
2023-04-28 12:31:14 +0300

    added codewars tsks folder

---
+++ JavaScript/Codewars/DNAtoRNA.js
@@ -0,0 +1,31 @@
+function DNAtoRNA(dna) {
+  // create a function which returns an RNA sequence from the given DNA sequence
+  if (!dna.includes("T")) {
+    return dna;
+  }
+  
+  let lastPosition = dna.indexOf("T");
+  let length = dna.length;
+
+  while (dna.includes("T", lastPosition)) {
+
+    for (let i = 0; i < length; i++) {
+      
+      if (i == lastPosition) {
+        dna += "U";
+      } else {
+        dna += dna.at(i);
+      }
+
+    }
+
+    dna = dna.slice(length);
+    lastPosition = dna.indexOf("T");
+
+  }
+  
+  
+  return dna;
+}
+
+console.log(DNAtoRNA("234T234"));
