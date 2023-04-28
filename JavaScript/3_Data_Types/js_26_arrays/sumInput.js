d0d681ebfe2d75d53fb51aec16835087929cabfe
Klymenko Nazariy <nazariy.klymenko@gmail.com>
2023-04-28 13:02:48 +0300

    sumInput task done

--- JavaScript/3_Data_Types/js_26_arrays/sumInput.js
+++ JavaScript/3_Data_Types/js_26_arrays/sumInput.js
@@ -1,1 +1,22 @@
 "use strict";
+
+let sum = sumInput();
+alert(sum);
+
+function sumInput() {
+  let resultArr = [];
+  let result = 0;
+  let inputNum = prompt("Введіть число:", "");
+
+  while (isFinite(inputNum) && !!inputNum) {
+    resultArr.push(+inputNum);
+    inputNum = prompt("Введіть число:", "");
+  }
+
+  for (let num of resultArr) {
+    result += num;
+  }
+
+  return result;
+
+}
