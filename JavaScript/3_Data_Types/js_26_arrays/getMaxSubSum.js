4095d358a8c8f195fb4629cbac6ba9b9aec0d666
Klymenko Nazariy <nazariy.klymenko@gmail.com>
2023-04-28 14:52:44 +0300

    getMaxSubSum task done

---
+++ JavaScript/3_Data_Types/js_26_arrays/getMaxSubSum.js
@@ -0,0 +1,50 @@
+"use strict";
+
+console.log(getMaxSubSum([100, -9, 2, -3, 5]));
+
+function getMaxSubSum(numbers) {
+  
+  let currentSum = 0;
+  let maxSubSum = 0;
+
+  for (let number of numbers) {
+    currentSum += number;
+    maxSubSum = Math.max(maxSubSum, currentSum);
+    currentSum = currentSum < 0 ? 0 : currentSum;
+  }
+
+  return maxSubSum;
+}
+
+/* function getMaxSubSum(numbers) { // O(n^2)
+  
+  let negativeCount = 0;
+  for (let number of numbers) {//All negative numbers check
+
+    if (number < 0) {
+      negativeCount++;
+    }
+
+    if (negativeCount == numbers.length) {
+      return 0;
+    }
+  }
+  
+  if (!numbers.length) {
+    return 0;
+  }
+
+  let maxSubSum = numbers[0];
+
+  for (let i = 0; i < numbers.length; i++){
+    let sum = 0;
+    
+    for (let j = i; j < numbers.length; j++){
+      sum += numbers[j];
+      maxSubSum = Math.max(maxSubSum, sum);
+    }
+
+  }
+
+  return maxSubSum;
+} */
