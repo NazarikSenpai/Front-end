3b505273b68facfdada3bdf8dbc07595bea50dc4
Klymenko Nazariy <nazariy.klymenko@gmail.com>
2023-05-02 14:23:33 +0300

    js 27 arrays methods continue

--- JavaScript/3_Data_Types/js_27_arrays_methods/arrays_methods.js
+++ JavaScript/3_Data_Types/js_27_arrays_methods/arrays_methods.js
@@ -58,3 +58,78 @@ arr.forEach((value, index, array) => {
   console.log(`value: ${value}, index: ${index}, array: ${array.join(" | ")}`);
 });//Перебирає елементи масиву і нічого не повертає */
 
+/* indexOf includes */
+
+/* let arr = ["Test", "QA", "Bug", NaN, "Report", "Butterfly", 0, "", false];
+
+console.log(arr.indexOf("QA", 1));
+console.log(arr.indexOf("xxx"));//Повертає -1 бо не знаходить
+console.log(arr.lastIndexOf(NaN));//повертає -1
+console.log(arr.includes(NaN));//includes може знайти NaN на відміну від indexOf
+console.log(arr.includes("Butterfly", 3));
+
+console.log(arr.lastIndexOf(false, -5));//ці всі 3 методи використовують строге порівняння === */
+
+/* find findIndex findLastIndex */
+
+/* let users = [
+  { id: 1, name: "Jo", lastName: "Jostar" },
+  { id: 2, name: "Marla", lastName: "Zinger" },
+  { id: 3, name: "Simon", lastName: "Gurren" },
+  { id: 4, name: "Kamino", lastName: "Lagann" },
+  { id: 1, name: "Jo", lastName: "Jostar" },
+];
+
+let currentUser = users.find(item => item.name == "Simon");
+
+console.log(currentUser.id, currentUser.name, currentUser.lastName);
+
+//let currentUserIndex = users.findIndex(user => user.name == "Jo");
+let currentUserIndex = users.findLastIndex(user => user.name == "Jo");//прийшлось обновити ноду щоб цей метод запрацював
+console.log("currentUserIndex:", currentUserIndex); */
+
+/* filter */
+
+/* let users = [
+  { id: 1, name: "Jo", lastName: "Jostar" },
+  { id: 2, name: "Marla", lastName: "Zinger" },
+  { id: 3, name: "Simon", lastName: "Gurren" },
+  { id: 4, name: "Kamino", lastName: "Lagann" },
+  { id: 1, name: "Jo", lastName: "Jostar" },
+];
+
+let jostarUsers = users.filter(user => user.lastName == "Jostar");//працює схоже з find але шукає всі елементи а не один перший
+console.log(jostarUsers); */
+
+/* Перетворення масиву map */
+
+/* let names = ["Jonathan", "Anisim", "Nika", "Nazariy", "Vlad"];
+
+let namesLengths = names.map(name => name.length);//повертає масив результатів виконання функції до кожного елемента в масиві
+console.log(namesLengths); */
+
+/* sort(fn) */
+
+/* let numValues = [3, 24, 5, 45, 10, 6, 3, 2];
+
+console.log(numValues.sort());//Сортування відбулося з перетворенням у строку
+console.log(numValues);
+
+function numCompare(a, b) {
+  if (a > b) return 1;
+  if (a == b) return 0;
+  if (a < b) return -1;
+}
+
+console.log(numValues.sort(numCompare));//тепер сортування відбулося правильно
+
+console.log(numValues.sort((a, b) => {
+  console.log(a, " ? ", b);
+  return a - b;
+}));//більш короткий запис функції порівняння;
+
+let names = ["Jonathan", "Anisim", "Nika", "Nazariy", "Vlad"];
+names.sort((a, b) => a.localeCompare(b));//Краще робити так зі строками щоб запобігти помилок зі специфічними буквами
+console.log(names);
+
+console.log(names.sort()); */
