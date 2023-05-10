1d6c261ac69698bd7df2ea8206e27c214fdcbbaf
Klymenko Nazariy <nazariy.klymenko@gmail.com>
2023-05-10 12:31:58 +0300

    groupById done

---
+++ JavaScript/3_Data_Types/js_27_arrays_methods/tasks/groupById.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let users = [
+  { id: "john", name: "John Smith", age: 20 },
+  { id: "ann", name: "Ann Smith", age: 24 },
+  { id: "pete", name: "Pete Peterson", age: 31 },
+];
+
+let usersById = groupById(users);
+console.log(usersById);
+
+function groupById(users) {
+  return users.reduce((usersById, user) => {
+    usersById[user.id] = user;
+    return usersById;
+  }, {});
+
+  /* let usersById = {}; //моє перше рішення
+
+  users.forEach(user => { usersById[user.id] = user; });
+
+  return usersById; */
+}
