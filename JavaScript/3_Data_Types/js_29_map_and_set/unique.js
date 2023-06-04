1861da097b5a4d091bd700590c6abe146fc03419
Klymenko Nazariy <nazariy.klymenko@gmail.com>
2023-06-04 18:28:44 +0300

    added prettier

--- JavaScript/3_Data_Types/js_29_map_and_set/unique.js
+++ JavaScript/3_Data_Types/js_29_map_and_set/unique.js
@@ -1,14 +1,21 @@
-"use strict";
+'use strict';
 
-let values = ["Hare", "Krishna", "Hare", "Krishna",
-  "Krishna", "Krishna", "Hare", "Hare", ":-O"
+let values = [
+  'Hare',
+  'Krishna',
+  'Hare',
+  'Krishna',
+  'Krishna',
+  'Krishna',
+  'Hare',
+  'Hare',
+  ':-O',
 ];
 
 console.log(unique(values));
 
 function unique(arr) {
-
-  return Array.from(new Set(arr));//більш лаконічний варіант з рішення
+  return Array.from(new Set(arr)); //більш лаконічний варіант з рішення
 
   /* let uniques = new Set();
   arr.forEach(value => {
