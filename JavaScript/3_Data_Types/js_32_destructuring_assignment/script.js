5487b8393e9e3e13a41b20ad13cfb4b314fb3018
“foxstudent105201” <nazariy.klymenko@gmail.com>
2023-06-06 19:01:07 +0300

    continue

--- JavaScript/3_Data_Types/js_32_destructuring_assignment/script.js
+++ JavaScript/3_Data_Types/js_32_destructuring_assignment/script.js
@@ -1,4 +1,44 @@
-"use strict";
+'use strict';
 
 /* destructuring array */
 
+/* let user = new Set(['Nazariy', 'Oleksandrovych', 'Klymenko']);//можна використовувати будь-який ітерований об'єкт
+
+let [firstName, , lastName] = user;//пустий елемент зліва пропускає елемент в масиві
+
+console.log(firstName);
+console.log(lastName);
+
+let newUser = {};
+
+[newUser.firstName, newUser.lastName] = user;
+
+console.log(newUser);
+
+let absoluteNewUser = {
+  name: 'John',
+  age: 30,
+};
+
+for (let [key, value] of Object.entries(absoluteNewUser)) {
+  console.log(key, value);
+}
+
+let admin = "Pasha";
+let currentUser = "John";
+
+[admin, currentUser] = [currentUser, admin];
+console.log(admin);
+console.log(currentUser); */
+
+/* rest and default parameters */
+
+let [firstThing, secondThing, ...rest] = "Hello, my friends. Nice to meet you".split(" ");
+
+console.log(firstThing);
+console.log(secondThing);
+console.log(rest);
+
+let [name = "name", surname = "surname"] = ["Nazariy"];
+console.log(name);
+console.log(surname);
